043c800369e81c4672e1052126f68e66f6401beb
rachelferrer <rferrer@beaconschool.org>
2019-03-30 14:44:30 +0000

    hi

--- warmupDay6.py
+++ warmupDay6.py
@@ -1,1 +1,10 @@
 # File name: warmupDay6.py
+print ("guess my secret number")
+answer = 0
+while answer < 12:
+    print (answer)
+    answer += 2
+answer = 2
+while answer < 100:
+    print (answer)
+    answer += 6
